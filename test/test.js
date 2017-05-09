784d9f19389679dc1985a0c0db0b9293df8fb2e5
Matt Thomson <red.cataclysm@gmail.com>
2017-05-09 14:12:26 -0400

    Get and InsertAfter

--- test/test.js
+++ test/test.js
@@ -1,5 +1,19 @@
 import Domo from 'DOMObject';
 
-var results = new Domo(document.getElementById('results'));
+// function heading (title, results) {
+//   var h1 = document.createElement('h1');
+//   h1.appendChild(document.createTextNode(title));
+//   results.appendChild(h1);
+// }
 
+var results = new Domo(document.getElementById('results'));
 console.log(results);
+var node = new Domo('div > ul > li * 5');
+var text = new Domo('p {foo}');
+var text2 = new Domo('p {bar}');
+console.log(node);
+// heading('Creation String Single', results);
+results.insertAfter(node);
+results.insertAfter(text, 'ul > li');
+results.insertAfter(text2, 'ul > li', { index: 2 });
+results.insertAfter(new Domo('p {baz}'), 'ul > li', { index: [1, 2, 3] });
