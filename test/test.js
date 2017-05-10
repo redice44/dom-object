0930326ab6ec06c3e3cc73561e81337902e8a61e
Matt Thomson <red.cataclysm@gmail.com>
2017-05-10 16:03:52 -0400

    Implements: Class Toggle

--- test/test.js
+++ test/test.js
@@ -9,12 +9,7 @@ import Domo from 'DOMObject';
 var results1 = new Domo(document.getElementById('results1'));
 var results2 = new Domo(document.getElementById('results2'));
 
-var node1 = new Domo('div > ul > li * 5');
-var node1 = new Domo('div > ul > li * 5');
-var text = new Domo('{foo}');
-var text2 = new Domo('{bar}');
-
-results1.append(new Domo('div > ul > li * 5'));
+results1.append(new Domo('div > ul > li * 5').addClass('yo'));
 results1.append(new Domo('{foo}'), 'ul > li').addClass('foo');
 results1.append(new Domo('{bar}'), 'ul > li', { index: 2 }).addClass('bar');
 results1.append(new Domo('{baz}'), 'ul > li', { index: [1, 2, 3] }).addClass('baz');
@@ -24,6 +19,9 @@ results1.insertBefore(new Domo('li {Inserted Before}'), 'ul > li', { index: [2, 
 results1.addClass('red', 'ul > li', { index: 4 }).addClass('yellow');
 results1.addClass(['blue', 'green'], 'ul > li', { index: 3 }).addClass('red');
 results1.removeClass('red', 'ul > li', { index: 4 });
+results1.toggleClass('foo', 'ul > li', { index: 0 });
+results1.toggleClass(['foo', 'bar'], 'ul > li', { index: 1 }).toggleClass('foo');
+results1.toggleClass(['blue', 'green'], 'ul > li', { index: 3 });
 
 results2.append(new Domo('div > ul > li * 5'));
 results2.get('ul > li').append(new Domo('{foo}'));
@@ -36,3 +34,6 @@ results2.get('ul > li', { index: 3 }).addClass(['blue', 'green', 'yellow']);
 results2.get('ul > li', { index: 4 }).addClass('red').addClass('blue');
 results2.get('ul > li', { index: 4 }).removeClass('red');
 results2.get('ul > li', { index: 3 }).removeClass(['green', 'yellow']);
+results2.get('ul > li', { index: 0 }).toggleClass('foo');
+results2.get('ul > li', { index: 1 }).toggleClass(['foo', 'bar']).toggleClass('foo');
+results2.get('ul > li', { index: 3 }).toggleClass(['blue', 'green']);
